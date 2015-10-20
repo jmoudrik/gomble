c82ac017580074ab8fa2ca3ca605bdaf1da645e0
Joe Moudrik <j.moudrik@gmail.com>
2015-10-20 17:52:37 +0200

    runpachi: use ENV variables to configure

--- runpachi.sh
+++ runpachi.sh
@@ -3,24 +3,35 @@
 #trap 'kill $PID_D 2>/dev/null' EXIT SIGINT SIGKILL
 
 run_slave () {
-	nice ./pachi -e uct -g localhost:1234 -l localhost:1235 "${SLAVE}${ALL}" &
+	nice ./pachi -e uct -g localhost:1234 -l localhost:1235 "${SLAVE_SETUP}${ALL}" &
 	NEWPID=$!
 }
 
 run_dist () {
-	./pachi -e distributed ${DIST}${ALL} 2>> ${LOGDIR}/LOG_pachi <&0 &
+	./pachi -e distributed ${DIST_SETUP}${ALL} 2>> ${LOGDIR}/LOG_pachi <&0 &
 	NEWPID=$!
 }
 
-LOGDIR="$(pwd)"
-NUM_SLAVES=2
+[ -z "$LOGDIR" ] && LOGDIR="$(pwd)"
+[ -z "$NUM_SLAVES" ] && NUM_SLAVES="2"
+[ -z "$PACHI_DIR" ] && PACHI_DIR=~/prj/pachi
+[ -z "$ALL" ] && export ALL=""
+[ -z "$SLAVE_SETUP" ] && SLAVE_SETUP="threads=2,max_tree_size=3072,slave"
+[ -z "$DIST_SETUP" ] && DIST_SETUP="slave_port=1234,proxy_port=1235"  # pass_all_alive"
 
+cd $PACHI_DIR
 exec 2>${LOGDIR}/LOG_runpachi
 
-cd ~/prj/pachi
-export ALL=""
-export SLAVE="threads=2,max_tree_size=3072,slave"
-export DIST="slave_port=1234,proxy_port=1235"  # pass_all_alive"
+cat >&2 <<EOF 
+$0: settings
+LOGDIR=$LOGDIR
+NUM_SLAVES=$NUM_SLAVES
+PACHI_DIR=$PACHI_DIR
+ALL=$ALL
+SLAVE_SETUP=$SLAVE_SETUP
+DIST_SETUP=$DIST_SETUP
+
+EOF
 
 run_dist 		#sets NEWPID
 echo "started master $NEWPID" >&2
