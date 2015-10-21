cd170159e820b35ba058107e6e662359f1125966
Joe Moudrik <j.moudrik@gmail.com>
2015-10-21 12:12:46 +0200

    runpachi: specify ports, logfile

--- runpachi.sh
+++ runpachi.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-#trap 'kill $PID_D 2>/dev/null' EXIT SIGINT SIGKILL
+trap 'kill $PID_D 2>/dev/null' EXIT SIGINT SIGKILL
 
 run_slave () {
-	nice ./pachi -e uct -g localhost:1234 -l localhost:1235 "${SLAVE_SETUP}${ALL}" &
+	( nice ./pachi -e uct -g localhost:${SLAVE_PORT} -l localhost:${LOG_PORT} "${SLAVE_SETUP}" ) &
 	NEWPID=$!
 }
 
 run_dist () {
-	./pachi -e distributed ${DIST_SETUP}${ALL} 2>> ${LOGDIR}/LOG_pachi <&0 &
+	./pachi -e distributed ${DIST_PORTS}${DIST_SETUP} 2>> $LOGFILE <&0 &
 	NEWPID=$!
 }
 
-[ -z "$LOGDIR" ] && LOGDIR="$(pwd)"
+[ -z "$LOGFILE" ] && LOGFILE="$(pwd)/LOG_pachi"
 [ -z "$NUM_SLAVES" ] && NUM_SLAVES="2"
 [ -z "$PACHI_DIR" ] && PACHI_DIR=~/prj/pachi
-[ -z "$ALL" ] && export ALL=""
+[ -z "$SLAVE_PORT" ] && SLAVE_PORT=1234
+[ -z "$LOG_PORT" ] && LOG_PORT=1235
 [ -z "$SLAVE_SETUP" ] && SLAVE_SETUP="threads=2,max_tree_size=3072,slave"
-[ -z "$DIST_SETUP" ] && DIST_SETUP="slave_port=1234,proxy_port=1235"  # pass_all_alive"
+[ -z "$DIST_SETUP" ] && DIST_SETUP=""  # pass_all_alive"
+
+# need to separate DIST_PORTS and DIST_SETUP when run_dist
+[ -n "$DIST_SETUP" ] && [ ! $( echo "$DIST_SETUP" | grep -e "^," ) ] && DIST_SETUP=",$DIST_SETUP"
+
+DIST_PORTS="slave_port=${SLAVE_PORT},proxy_port=$LOG_PORT"
 
 cd $PACHI_DIR
-exec 2>${LOGDIR}/LOG_runpachi
+exec 2> $LOGFILE
 
 cat >&2 <<EOF 
-$0: settings
-LOGDIR=$LOGDIR
+
+$0: $(date)
+LOGFILE=$LOGFILE
 NUM_SLAVES=$NUM_SLAVES
 PACHI_DIR=$PACHI_DIR
-ALL=$ALL
 SLAVE_SETUP=$SLAVE_SETUP
 DIST_SETUP=$DIST_SETUP
 
@@ -36,6 +42,8 @@ EOF
 run_dist 		#sets NEWPID
 echo "started master $NEWPID" >&2
 PID_D=$NEWPID
+sleep 1
+#kill -0 $PID_D 2>/dev/null || { echo "could not start master, exiting" ; exit 1 ; }
 
 SLAVES=
 for num in `seq $NUM_SLAVES` ; do
@@ -45,15 +53,15 @@ for num in `seq $NUM_SLAVES` ; do
 done
 
 while true ; do
-	echo "" >&2
+	#echo "" >&2
 	## check distributed
 	kill -0 $PID_D 2>/dev/null && {
-		echo "master $PID_D alive" >&2
+		#echo "master $PID_D alive" >&2
 		# running
 		NEW_SLAVES=
 		for PID in $SLAVES ; do
 			kill -0 $PID 2>/dev/null && {
-				echo "slave $PID alive" >&2
+				#echo "slave $PID alive" >&2
 				NEWPID=$PID
 			} || {
 				run_slave		#sets NEWPID
@@ -65,7 +73,6 @@ while true ; do
 	} || {
 		echo "master $PID_D dead" >&2
 		# dead
-		sleep 1
 		echo "killing all the slaves" >&2
 		for PID in $SLAVES ; do
 			kill -0 $PID 2>/dev/null && {
