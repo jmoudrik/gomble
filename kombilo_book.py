1c7ae01c92023e9fe90cafa0110af59e0d315c61
Joe Moudrik <j.moudrik@gmail.com>
2015-10-29 22:02:46 +0100

    kombilo_book: clean up, better names

--- kombilo_book.py
+++ kombilo_book.py
@@ -32,31 +32,26 @@ def binom_integrate(n, k, fr, to):
     return bp.sum() / INTEGRATE_SAMPLES
 
 
-def log_score(n, k):
+def log_diff_binom_integrate(n, k):
     # faster (and a bit more precise)
     # alternative
     bp = binomial(n, k, INTEGRATE_LINSPACE)
     lo = INTEGRATE_LINSPACE_LOW * bp
     hi = INTEGRATE_LINSPACE_HIGH * bp
 
-    return (log(lo.sum() / INTEGRATE_SAMPLES)
-            - log(hi.sum() / INTEGRATE_SAMPLES))
+    return log(lo.sum()) - log(hi.sum())
 
 
-def int_score_neg(c):
+def score_int_neg(c):
     return binom_integrate(c.samples, c.wins, 0.0001, 0.5)
 
 
-def int_score_pos(c):
+def score_int_pos(c):
     return binom_integrate(c.samples, c.wins, 0.5, 0.9999)
 
 
-def int_score_log(c):
-    return log(int_score_neg(c)) - log(int_score_pos(c))
-
-
-def int_score_log_fast(c):
-    return log_score(c.samples, c.wins)
+def score_diff_log_int(c):
+    return log_diff_binom_integrate(c.samples, c.wins)
 
 
 def add_prior(c):
@@ -220,16 +215,28 @@ def cont_test():
           ('black', 'nahodny', 2, 2),
           ('black', 'nahodny vetsi', 3, 3),
           ('black', 'nadprumerny', 26, 15),
+          ('black', 'nejlepsi', 100, 60),
+          ('black', 'prumer', 10000, 5000),
          ]
 
     return [Cont(*t) for t in l]
 
 
+def test_speed():
+    c = Cont('black', 'prumer', 10000, 5000)
+
+    for a in xrange(1000):
+        #s = score_int_neg(c)
+        #s = score_int_pos(c)
+        s = score_diff_log_int(c)
+
+
 def main():
     def score(c):
         cc = add_prior(c)
-        # return int_score_log(cc)
-        return int_score_log_fast(cc)
+        # return score_int_neg(c)
+        # return score_int_pos(c)
+        return score_diff_log_int(cc)
 
     cs = cont_test()
     # cs = cont_from_pat()
@@ -243,15 +250,13 @@ def main():
     s = sorted(cs, key=score)
     for c in s:
         print repr(c)
-        # print "< 0.5  ", int_score_neg(c)
-        # print "< 0.5 P", int_score_neg(add_prior(c))
-        # print "> 0.5  ", int_score_pos(c)
-        # print "> 0.5 P", int_score_pos(add_prior(c))
-        print "log   P", int_score_log(add_prior(c))
-        print "log F P", int_score_log_fast(add_prior(c))
-        # print "log    ", int_score_log(c)
+        print "< 0.5 P", score_int_neg(add_prior(c))
+        print "> 0.5 P", score_int_pos(add_prior(c))
+        print "diff  P", score_diff_log_int(add_prior(c))
 
 
 if __name__ == "__main__":
     main()
+
+    # test_speed()
     pass
